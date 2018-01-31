3c9cba71782ad7a50b478505b39caa7efeebb378
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2018-01-31 00:55:33 -0600

    fix: modify json data
    Work with js functions to consume json

--- client/main.js
+++ client/main.js
@@ -1,32 +1,26 @@
-var product = [{
-  id: 1,
-  title: 'hamburguesa',
-  image: 'https://cdn3.cnet.com/img/zti-8maGznHRwm0Q6af0m7InwzM=/fit-in/970x0/2017/03/22/1c848061-a343-460a-a044-b07cb94e7927/if-burger.jpg',
-  description: 'Mega hamburguesa',
-  price: '100.00'
-},{
-  id: 2,
-  title: 'hamburguesa',
-  image: 'https://cdn3.cnet.com/img/zti-8maGznHRwm0Q6af0m7InwzM=/fit-in/970x0/2017/03/22/1c848061-a343-460a-a044-b07cb94e7927/if-burger.jpg',
-  description: 'Mega hamburguesa',
-  price: '100.00'
-}]
-
-var prod2 = JSON.parse('https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json');
+var product = [
+  {
+    "id": "0",
+    "title": 'Hamburguesa',
+    "image": "https://cdn3.cnet.com/img/zti-8maGznHRwm0Q6af0m7InwzM=/fit-in/970x0/2017/03/22/1c848061-a343-460a-a044-b07cb94e7927/if-burger.jpg",
+    "description": "Mega hamburgues",
+    "price": "10.00",
+  },
+]
 
 console.log(product);
-render(prod2);
+render(product);
 
 
 function render(){
   var html = product.map(function(message,index){
     return(`
         <div class="message">
-          <p>${message.id}</p>
-          <p>${message.title}</p>
-          <p>${message.image}</p>
-          <p>${message.description}</p>
-          <p>${message.price}</p>
+          <p><strong>Id </strong>${message.id}</p>
+          <p><strong>Producto </strong>${message.title}</p>
+          <img src="${message.image}" alt="Product image" style="width:300px; height:auto;">
+          <p><strong>Descripción </strong>${message.description}</p>
+          <p><strong>Precio $</strong>${message.price}</p>
         </div>
       `);
   }).join('  ');
